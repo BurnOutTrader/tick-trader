4b4fea6c5eb23dc39a958c238de5c323c2003c61
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2025-10-11 13:40:04 +0200

    add support for all known projectX tenants

--- crates/database/src/paths.rs
+++ crates/database/src/paths.rs
@@ -4,6 +4,7 @@ use tt_types::providers::ProviderKind;
 /// This should be considered part of the storage contract; changing it would
 /// require a migration.
 pub fn provider_kind_to_db_string(provider: ProviderKind) -> String {
+    //dont change, we dont want the tenants here only the ProviderKind eg ProjectX or Rithmic not their wrapped enums for specific firms
     match provider {
         ProviderKind::ProjectX(_) => "projectx".to_string(),
         ProviderKind::Rithmic(_) => "rithmic".to_string(),
