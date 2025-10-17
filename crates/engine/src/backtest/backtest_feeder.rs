5c67e5e8f9cd8a93441af13857436e8283bca753
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2025-10-17 10:45:57 +1000

    minor

--- crates/engine/src/backtest/backtest_feeder.rs
+++ crates/engine/src/backtest/backtest_feeder.rs
@@ -450,7 +450,7 @@ impl BacktestFeeder {
                         let _ = bus.broadcast(Response::BarBatch(batch));
                     }
                 }
-                await_ack(notify).await; //todo! possibly notify is no longer needed for backtest sync
+                //await_ack(notify).await; //todo! possibly notify is no longer needed for backtest sync
             }
 
             // Main loop: interleave handling of requests with emitting events in time order
